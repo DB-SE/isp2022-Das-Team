d7e113a0f11a30c530aa3f3bf24c74e9e7387feb
F-erdi <ferdinand_wild@gmx.de>
2023-01-08 21:47:39 +0100

    remaining changes

--- Task3/src/interfaces/IGraphAlgo.java
+++ Task3/src/interfaces/IGraphAlgo.java
@@ -8,10 +8,8 @@ import src.Edge;
 import src.IGraph;
 import src.Node;
 
-//Mst, Export Adjacency
 public interface IGraphAlgo {
 	
-	//FixMe: probably better to handle them in separate interfaces ?
 	ConcreteGraph mst();
 	int[][] getAdjacencyMatrix ();
 	boolean isDirected();
